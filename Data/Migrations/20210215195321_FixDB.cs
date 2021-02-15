30cc27c1002f778026b03b3d1f406e094e01f0fd
Dmitriy <dmitriy1442@yandex.ru>
2021-02-15 23:56:46 +0400

    FixedErrors

---
+++ Data/Migrations/20210215195321_FixDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreStoreMVC.Data.Migrations
+{
+    public partial class FixDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
